2e65213c5fc5bb17e1657d15d3844139a5b6fc85
Blejzer <nikola.kujaca@gmail.com>
2014-07-10 17:00:06 +0200

    zavrsio signupcontroller

--- zavrsniRadVMS1851/src/main/java/ba/fit/vms/signup/SignupController.java
+++ zavrsniRadVMS1851/src/main/java/ba/fit/vms/signup/SignupController.java
@@ -1,11 +1,19 @@
 package ba.fit.vms.signup;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ba.fit.vms.config.KorisnickiServis;
+import ba.fit.vms.pojo.Korisnik;
 import ba.fit.vms.repository.KorisnikRepository;
+import ba.fit.vms.support.web.PorukaHelper;
 
 @Controller
 public class SignupController {
@@ -21,6 +29,21 @@ public class SignupController {
 		return new SignupForm();
 	}
 	
-	
+	/********************************************
+	 * 					Mapiranje				*
+	*********************************************/
 
+	@RequestMapping(value = "signup", method = RequestMethod.POST)
+	public String signup(@Valid @ModelAttribute SignupForm signupForm, Errors errors, RedirectAttributes ra) {
+		if (errors.hasErrors()) {
+			return null;
+		}
+		
+		Korisnik korisnik = korisnikRepository.save(signupForm.kreirajKorisnika());
+		korisnickiServis.signin(korisnik);
+		
+		PorukaHelper.addSuccessAttribute(ra, "Congratulations! You have successfully signed up.");
+		
+		return "redirect:/";
+	}
 }
