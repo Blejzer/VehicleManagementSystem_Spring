1a0cb9816152f079d33c85ca189611c863d600d1
Blejzer <nikola.kujaca@gmail.com>
2014-08-27 16:29:41 +0200

    front view: Lista korisnika - izgled, poruke filters - sredio izgled pretrazivaca za korisnike
    backend:
    KorisnikWebController - mapirao listu korisnika, get i post novi
    korisnik, dodao opis

--- zavrsniRadVMS1851/src/main/java/ba/fit/vms/controllers/KorisnikWebController.java
+++ zavrsniRadVMS1851/src/main/java/ba/fit/vms/controllers/KorisnikWebController.java
@@ -2,6 +2,7 @@ package ba.fit.vms.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.hibernate.SessionException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.beans.support.PagedListHolder;
 import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -32,8 +35,15 @@ public class KorisnikWebController {
     }
 	
 	
+	/**
+	 * Mapiramo listu svih korisnika na dvije adrese '/admin/korisnici/ i /admin/korisnici/lista'
+	 * @param request
+	 * @param response
+	 * @param model
+	 * @return
+	 */
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	@RequestMapping(value = "/admin/korisnici/", method = RequestMethod.GET)
+	@RequestMapping(value = {"/admin/korisnici/", "/admin/korisnici/lista"}, method = RequestMethod.GET)
 	public String getSviKorisnici(HttpServletRequest request, HttpServletResponse response, Model model){
 		
 		if(request.getParameter("page")==null)
@@ -43,7 +53,7 @@ public class KorisnikWebController {
 			request.getSession().setAttribute("KorisnikWebController_korisnici", korisnici);
 			model.addAttribute("pager", korisnici);
 
-			return "/admin/korisnik/list";
+			return "/admin/korisnik/lista";
 		}	
 		else 
 		{
@@ -62,13 +72,46 @@ public class KorisnikWebController {
 		}
 	}
 	
-	
+
+	/**
+	 * Mapiramo otvaranje forme za dodavanje novog korisnika
+	 * prije vracanja forme, provjeravamo da li korisnik ima permisije 'isUserInRole("ROLE_ADMIN")'
+	 * @param request
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value="/admin/korisnici/novi", method = RequestMethod.GET)
 	public String getNoviKorisnik(SecurityContextHolderAwareRequestWrapper request, Model model){
 		
 		if(request.isUserInRole("ROLE_ADMIN")){
 			model.addAttribute("korisnikAtribut", new Korisnik());
 			return "/admin/korisnik/novi";
+			
+		}
+		return "/auth/zabranjenPristup";
+	}
+	
+	
+	/**
+	 * Provjeravamo da li korisnik ima permisije
+	 * Mapiramo snimanje podataka o novom korisniku dobivenih iz forme
+	 * ukoliko Korisnik nije validan, vraca na formu i ukazuje na gresku
+	 * snimamo novog korisnika i preusmjeravamo na listu korisnika
+	 * @param korisnik
+	 * @param rezultat
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value="/admin/korisnici/novi", method = RequestMethod.POST)
+	public String postNoviKorisnik(@ModelAttribute("korisnikAtribut") @Valid Korisnik korisnik, BindingResult rezultat, SecurityContextHolderAwareRequestWrapper request){
+		if(request.isUserInRole("ROLE_ADMIN")){
+			if (rezultat.hasErrors()) 
+			{		
+				return "/admin/korisnik/dio/novi";
+			}
+			
+			korisnikRepository.save(korisnik);
+			return "redirect:/admin/korisnici/";
 			
 		}
 		return "/auth/zabranjenPristup";
