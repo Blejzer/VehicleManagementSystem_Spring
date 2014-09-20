737ecd2a9a39f33925457b417656eebddfdf10e7
Blejzer <nikola.kujaca@gmail.com>
2014-09-20 18:23:00 +0200

    registracija novi.html

--- zavrsniRadVMS1851/src/main/java/ba/fit/vms/controllers/RegistracijaController.java
+++ zavrsniRadVMS1851/src/main/java/ba/fit/vms/controllers/RegistracijaController.java
@@ -1,8 +1,13 @@
 package ba.fit.vms.controllers;
 
+import javax.validation.Valid;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +21,8 @@ import ba.fit.vms.util.RegistracijaValidator;
 @Controller
 public class RegistracijaController {
 	
+	protected static Logger logger = Logger.getLogger("controller");
+	
 	@Autowired
 	private RegistracijaRepository registracijaRepository;
 	
@@ -26,18 +33,35 @@ public class RegistracijaController {
 	
 	
 	
-	@RequestMapping(value = "/admin/registrations/add", method = RequestMethod.GET)
+	@RequestMapping(value = "/admin/registracija/novi", method = RequestMethod.GET)
 	public String getAdd(@RequestParam(value="id", required=false) String vin, Model model) {
 
 		if(vin==null){
-			model.addAttribute("registrationAttribute", new Registracija());
+			model.addAttribute("registracijaAtribut", new Registracija());
 			model.addAttribute("regVehicles", voziloRepository.getNeregistrovanaVozila());
 		} else{
-			model.addAttribute("registrationAttribute", new Registracija());
+			model.addAttribute("registracijaAtribut", new Registracija());
 			model.addAttribute("regVehicles", voziloRepository.findByVin(vin));
 		}
 
-		return "/admin/registration/add";
+		return "/admin/vozila/registracija/novi";
+	}
+	
+	
+	@RequestMapping(value = "/admin/registracija/novi", method = RequestMethod.POST)
+	public String postAdd(@ModelAttribute("registrationAttribute") @Valid Registracija registracija,
+			BindingResult result) {
+		logger.debug("poceo post");
+		val.validate(registracija, result);
+		logger.debug("uradio validaciju");
+		Registracija reg = val.obrada(registracija, result);
+		logger.debug("obradio");
+		if(result.hasErrors()){
+			return "/admin/registracija/novi?vin="+registracija.getVozilo().getVin();
+		}
+		registracijaRepository.save(reg);
+		return "redirect:/admin/vozila/";
+		
 	}
 
 }
