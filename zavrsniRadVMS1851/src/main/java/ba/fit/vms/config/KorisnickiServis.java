94483e2f6d07199161ef626feb111ae1a63bd1a7
Blejzer <nikola.kujaca@gmail.com>
2016-02-11 09:44:02 +0100

    Ispravka Korisnickog servisa. Sada se lozinka hashira.

--- zavrsniRadVMS1851/src/main/java/ba/fit/vms/config/KorisnickiServis.java
+++ zavrsniRadVMS1851/src/main/java/ba/fit/vms/config/KorisnickiServis.java
@@ -2,7 +2,8 @@ package ba.fit.vms.config;
 
 import java.util.Collections;
 
-import javax.annotation.PostConstruct;
+// import javax.annotation.PostConstruct;
+// import javax.inject.Inject;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,6 +15,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+// import org.springframework.security.crypto.password.PasswordEncoder;
 
 import ba.fit.vms.pojo.Korisnik;
 import ba.fit.vms.repository.KorisnikRepository;
@@ -23,16 +25,25 @@ public class KorisnickiServis implements UserDetailsService {
 	@Autowired
 	private KorisnikRepository korisnikRepository;
 	
+	/**
+	 * Dependency injection pasword enkodera kako bi smo inicijalne korisnike mogli ispravno upisati u bazu
+	 **/ 
+	/* @Inject
+	private PasswordEncoder passwordEncoder;
+	
 	@PostConstruct	
 	protected void initialize() {
 		
-		/*Korisnik korisnikAdmin = new Korisnik("root", "Admin", "User", true, "toor", "ROLE_ADMIN");
+		Korisnik korisnikAdmin = new Korisnik("root", "Admin", "User", true, "toor", "ROLE_ADMIN");
 		Korisnik korisnikRegular = new Korisnik("korisnik", "Regular", "User", true, "korisnik", "ROLE_USER");
+		korisnikAdmin.setLozinka(passwordEncoder.encode(korisnikAdmin.getLozinka())); // vrsimo enkripciju lozinke
+		korisnikRegular.setLozinka(passwordEncoder.encode(korisnikRegular.getLozinka())); // vrsimo enkripciju lozinke
 		
 		korisnikRepository.save(korisnikAdmin);
-		korisnikRepository.save(korisnikRegular);*/
+		korisnikRepository.save(korisnikRegular);
 		
-	}
+		
+	} */
 
 	@Override
 	public UserDetails loadUserByUsername(String email)
